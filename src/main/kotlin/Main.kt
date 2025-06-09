c4abf96f849a40cd6e79bcbb3dc288ba8eb12107
jpaolo <paulghi@gmail.com>
2025-06-09 00:31:59 -0700

    feat: working mode0 - from Ally Activities page to InvestBook execution tab for current year

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,43 +2,50 @@ package org.home.prac.invest.book
 
 import org.home.prac.invest.book.mapper.ActivityMapper
 import org.home.prac.invest.book.models.Activity
+import org.home.prac.invest.book.models.ActivityType
 import org.home.prac.invest.book.util.getSplitsWithTrimming
 import org.home.prac.invest.book.util.readClipboardText
+import org.home.prac.invest.book.util.toInvestBookExecutionFromActivity
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
+/***
+ * mode=0: from Ally Activities page to InvestBook current year tab
+ * mode=1: from InvestBook current year tab to InvestBook summary activities
+ */
+fun main(args: Array<String>) {
     val clipboardContent = try {
         readClipboardText()
     } catch (e: Exception) {
         e.printStackTrace()
     }
 
-    if (clipboardContent != null) {
-        println("Clipboard content:")
-        println(clipboardContent)
-        val charCount = clipboardContent.toString().length
-        val charList = clipboardContent.toString().toCharArray()
-        println("$charCount characters")
-//        for ((i, char) in charList.withIndex()) {
-//            println("$i: $char; ascii: ${char.code}")
-//        }
-
-        // do real work (ascii-10 = NL)
-        val lines = getSplitsWithTrimming(clipboardContent.toString(), 10.toChar())
-        val activityMapper = ActivityMapper()
-        val activities = mutableListOf<Activity>()
-        for (line in lines) {
-            try {
-                val activity = activityMapper.fromAllyActivity(line)
-                activity?.let {
-                    activities.add(activity)
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
+    val lines = getSplitsWithTrimming(clipboardContent.toString(), 10.toChar())  // ascii-10 = NL
+    val activityMapper = ActivityMapper()
+    val activities = mutableListOf<Activity>()
+    for (line in lines) {
+        try {
+            val activity = activityMapper.fromAllyActivity(line)
+            activity?.let {
+                activities.add(activity)
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        println("activities: $activities")
+    }
+    if (clipboardContent != null) {
+        if (args[0] == "0") {   // mode=0: from Ally Activities page to InvestBook current year tab
+            val investBookExecutions = StringBuilder()
+            var row = args[1].toInt()
+            activities.filter { it.type == ActivityType.SOLD || it.type == ActivityType.BOUGHT }.forEach {
+                investBookExecutions.append(toInvestBookExecutionFromActivity(it, row++))
+                investBookExecutions.append(10.toChar()) // ascii-10 = NL
+            }
+            investBookExecutions.deleteCharAt(investBookExecutions.length - 1)
+            // TODO: to clipboard
+            println(investBookExecutions)
+        } else if (args[0] == "1") {    // mode=1: from InvestBook current year tab to InvestBook summary activities
+
+        }
+
     } else {
         println("No text found on the clipboard or an error occurred.")
     }
--- src/main/kotlin/util/TextExtraction.kt
+++ src/main/kotlin/util/TextExtraction.kt
@@ -1,5 +1,38 @@
 package org.home.prac.invest.book.util
 
+import org.home.prac.invest.book.models.Activity
+import org.home.prac.invest.book.models.ActivityType
+import java.lang.IllegalStateException
+import java.time.format.DateTimeFormatter
+
 fun getSplitsWithTrimming(input: String, separator: Char): List<String> {
     return input.split(separator).map { it.trim() }
+}
+
+fun toInvestBookExecutionFromActivity(activity: Activity, curRow: Int): String {
+    val shareCol = "B"
+    val priceCol = "D"
+    val feeCol = "E"
+
+    val rowText = StringBuilder()
+    val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+    rowText.append(activity.date.format(formatter)).append('\t')
+    rowText.append(activity.shares).append('\t')
+    rowText.append("${activity.type.sourceName}: ${activity.symbol}").append('\t')
+    rowText.append(activity.price!!.formattedAmount).append('\t')
+    rowText.append(activity.fee!!.formattedAmount).append('\t')
+
+    when (activity.type) {
+        ActivityType.BOUGHT -> {
+            rowText.append("=ROUND(-$shareCol$curRow*$priceCol$curRow-$feeCol$curRow,2)")
+        }
+        ActivityType.SOLD -> {
+            rowText.append("=ROUND($shareCol$curRow*$priceCol$curRow-$feeCol$curRow,2)")
+        }
+        else -> {
+            throw IllegalStateException("${activity.type}")
+        }
+    }
+
+    return rowText.toString()
 }
